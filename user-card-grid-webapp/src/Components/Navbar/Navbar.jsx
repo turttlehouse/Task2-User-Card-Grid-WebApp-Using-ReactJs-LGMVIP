aea58b31070a5be3b3b2a372980126c4834f62ec
turttlehouse <buidandrun464@gmail.com>
2023-09-21 21:51:23 +0545

    Navbar.jsx -Declaring functional component called Navbar that can receive props as an input and returns the Navbar which contains the heading and Get Users button

--- user-card-grid-webapp/src/Components/Navbar/Navbar.jsx
+++ user-card-grid-webapp/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = (props) => {
 
 
   
   return (
-    <div>Navbar</div>
+    /* using React Fragments - Fragments enable us to return multiple elements from a component without wrapping them in a parent element. */
+    <>
+     <div className='Navbar'>
+        <h1>UserHub</h1>
+
+        <button className='get-users-btn' onClick={props.PassedasProps}>Get Users</button>
+    
+    </div>
+     
+    </>
   )
 }
 
 export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+Conditional Rendering: Fragments are handy for conditional rendering when we want to return different elements based on a condition.
+
+
+function Greeting({ isLoggedIn }) {
+  return (
+    <>
+      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+    </>
+  );
+}
+*/
