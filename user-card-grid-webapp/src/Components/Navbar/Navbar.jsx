e3142ccaf04bf79d4c41fe43b3f6c458e53b8689
turttlehouse <buidandrun464@gmail.com>
2023-09-21 22:09:22 +0545

    Navbar.css - styling the navbar component and body margin set to 0 in index.css

--- user-card-grid-webapp/src/Components/Navbar/Navbar.jsx
+++ user-card-grid-webapp/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Navbar.css'
+
+
 
 const Navbar = (props) => {
 
 
-  
+  /* 
+The Navbar functional component is returning a JSX structure, which represents the user interface (UI) elements that should be rendered by this component when it's used */
+
+  /* return (...): This block of code is the JSX (JavaScript XML) representation of the component's rendering. It defines what the component should render when it's used. */
+  /* JSX Javascript xml allows us to write in html-like code in js file */
   return (
     /* using React Fragments - Fragments enable us to return multiple elements from a component without wrapping them in a parent element. */
     <>
