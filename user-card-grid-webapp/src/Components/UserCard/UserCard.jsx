5bad2b87bf5b95efebc2a5292d6c9fd25efa45fa
turttlehouse <buidandrun464@gmail.com>
2023-09-21 23:17:05 +0545

    state variable 'users' is passed a prop in UserCard component and a returning a jsx stucture where state variable 'users' is done looping using map high order array method to iterate over id,avatar,name and email of object 'user'

--- user-card-grid-webapp/src/Components/UserCard/UserCard.jsx
+++ user-card-grid-webapp/src/Components/UserCard/UserCard.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
-const userCard = () => {
+const UserCard = ( {users}) => {
 
-  
+
   return (
-    <div>userCard</div>
-  )
-}
+    <div className='user-grid'>
+      {users.length > 0 ? (
 
-export default userCard
+        users.map((user) => (
+
+          <div className='user-card' key={user.id}>
+            <img src = {user.avatar} alt={`${user.first_name}'s avatar`} />
+            
+            <h2>{user.first_name} {user.last_name}</h2>
+
+            <p>Email: {user.email}</p> 
+          </div>
+        ))
+
+  ):(null)}
+
+      </div>
+  );
+};
+
+export default UserCard
+
+ /* accessing the email property of the user object  */
+
+
+/* `${}` within backticks is used for template literals. */
+
+/* {}it is not a template literal but These curly braces are used in JSX (JavaScript XML) to indicate that we are embedding JavaScript code within the HTML-like structure. Anything inside these braces is treated as JavaScript. */
